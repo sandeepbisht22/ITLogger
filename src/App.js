6190248a10aca09f98dca40fd54369c5c31291b0
Sandeep <bishtsandy22@gmail.com>
2021-09-28 09:23:59 +0530

    Added materialize and basic clean up of react and setup of react-json-server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import React, { useEffect } from "react";
+const App = () => {
+  return <div className="App">My APP</div>;
+};
 
 export default App;
